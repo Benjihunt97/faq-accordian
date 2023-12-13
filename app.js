d768f7b8238c4df51ba0f3226ac5a0f69b92a4f3
Benjihunt97 <73445088+Benjihunt97@users.noreply.github.com>
2023-12-13 18:38:19 +0000

    app.js

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 $(document).ready(() => {
 
-    $('.hidden-text').slideUp();
+$('.hidden-text').slideUp();
 
-    $(document).ready(function () {
-        $('.open-text').on('click', function () {
-            let hiddenText = $(this).closest('.app-item').find('.hidden-text');
-            let allIcons = $('.open-text');
+    $('.open-text').on('click', function () {
+        let hiddenText = $(this).closest('.app-item').find('.hidden-text');
+        let allIcons = $('.open-text');
 
-            // Close all other sections and update icons
-            $('.app-item').not($(this).closest('.app-item')).find('.hidden-text').slideUp(300);
-            allIcons.not(this).removeClass('fa-minus').addClass('fa-plus');
+        // Close all other sections and update icons
+        $('.app-item').not($(this).closest('.app-item')).find('.hidden-text').slideUp(300);
+        allIcons.not(this).removeClass('fa-minus').addClass('fa-plus');
 
-            // Toggle the clicked section and update its icon
-            hiddenText.slideToggle(300);
-            $(this).toggleClass('fa-plus fa-minus');
-        });
+        // Toggle the clicked section and update its icon
+        hiddenText.slideToggle(300);
+         $(this).toggleClass('fa-plus fa-minus');
     });
 });
